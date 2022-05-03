8279a47e703e9d8c2d0ea9da590248a821c2f50b
Hombre2014 <chamkoriyski@gmail.com>
2022-05-03 17:34:55 +0200

    Make it working!

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import HelloWorld from './HelloWorld'
 
 class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => ("Home!")} />
-          <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
-        </Switch>
+        <Route exact path="/" render={() => ("Home!")} />
+        <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
       </BrowserRouter>
     );
   }
