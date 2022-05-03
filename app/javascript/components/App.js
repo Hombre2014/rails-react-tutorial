ff6c98fcde35422077756dc95b85b669e9f823d6
Hombre2014 <chamkoriyski@gmail.com>
2022-05-03 18:19:52 +0200

    App is not working

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HelloWorld from './HelloWorld'
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Route exact path="/" render={() => ("Home!")} />
-        <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route exact path = "/" render={ () => ("Home!") } />
+          <Route path = "/hello" render={ () => <HelloWorld greeting="Friend"/> } />
+        </Routes>
+      </Router>
     );
   }
 }
